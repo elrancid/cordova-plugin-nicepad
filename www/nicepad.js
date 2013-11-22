9da72510f755e9c6d8bb2da19aff9cc06d246e14
Davide Zeffiro <davide.zeffiro@sportrfid.com>
2013-11-22 18:02:57 +0100

    Added quit, send and receive methods in JS

--- www/nicepad.js
+++ www/nicepad.js
@@ -44,12 +44,40 @@ function NicePad() {}
 /**
  * Echo a message
  *
- * @param {Function} successCallback The function to call when the echo message is available
- * @param {Function} errorCallback The function to call when there is an error getting the echo message
+ * @param {Function} successCallback The function to call when the echo message is available.
+ * @param {Function} errorCallback The function to call when there is an error getting the echo message.
  * @param {String} message The message to send.
  */
 NicePad.prototype.echo = function(successCallback, errorCallback, message) {
 	exec(successCallback, errorCallback, "NicePad", "echo", [message]);
+};
+
+/**
+ * Exit from webapp and return to notive app.
+ */
+NicePad.prototype.quit = function() {
+	exec(null, null, "NicePad", "quit", []);
+};
+
+/**
+ * Send a message over TCP connection.
+ *
+ * @param {Function} successCallback The function to call when the message is sent correctly.
+ * @param {Function} errorCallback The function to call when there is an error sending message.
+ * @param {String} message The message to send.
+ */
+NicePad.prototype.send = function(successCallback, errorCallback, message) {
+	exec(successCallback, errorCallback, "NicePad", "send", [message]);
+};
+
+/**
+ * Receive the messages from TCP connection.
+ *
+ * @param {Function} successCallback The function to call when the a message is received.
+ * @param {Function} errorCallback The function to call when there is an error receiving the message.
+ */
+NicePad.prototype.receive = function(successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "NicePad", "receive", []);
 };
 
 module.exports = new NicePad();
